d3a684d630117c4466b9c220005ff771ff1108df
askaborg <hienle@askaborg.com>
2020-02-06 11:55:38 -0800

    if done else

--- src/components/DisplayApod.js
+++ src/components/DisplayApod.js
@@ -2,53 +2,52 @@ import React from "react"
 import styled from "styled-components"
 import { Alert } from "reactstrap"
 
-const MyDiv = styled.div`
+const ApodDiv = styled.div`
 background: black;
 color: blue;
 height: 100%;
 width: 100%;
 padding: 160px;
 `
-const MyH1 = styled.h1`
+const ApodH1 = styled.h1`
 font-size: 5rem;
 `
-const MyDate = styled.p`
+const ApodDate = styled.p`
 font-size: 3rem;
 color: white;
 `
-const MyP = styled.p`
+const ApodP = styled.p`
 font-size: 1.5rem;
 display: flex;
 flex-direction: column;
 margin: 0% 20%;
 `
 function DisplayApod(props) {
+  const displayData = (apodData) => {
+    const {date, explanation, title, url} = apodData
 
-  const displayIt = (props) => {
-    const {date, explanation, title, url} = props.apodData
-    const {message} = props.apodError
-    console.log('date', date)
-    console.log('explanation', explanation)
-    console.log('title', title)
-    console.log('url', url)
-    console.log('message', message)
-
-  }
-  displayIt(props)
-
-
-  return <></>
-    // //true
-    // <Alert color="danger">{message}</Alert>
-    // ) : (
-    // //false
-    // <MyDiv className="apod-nasa" key={date}>
-    //   <img className="apod-image" alt={title} src={url}></img>
-    //   <MyH1>{title}</MyH1>
-    //   <MyDate>{date}</MyDate>
-    //   <MyP>{explanation}</MyP>
-    // </MyDiv> 
-    // )
+    return (
+      <ApodDiv className="apod-nasa" key={date}>
+        <img className="apod-image" alt={title} src={url}></img>
+        <ApodH1>{title}</ApodH1>
+        <ApodDate>{date}</ApodDate>
+        <ApodP>{explanation}</ApodP>
+      </ApodDiv> 
+    )
   }
 
-  export default DisplayApod
+  const displayError = (apodError) => {
+    const {message} = apodError
+
+    return (<Alert color="danger">{message}</Alert>)
+  }
+
+  if (props.apodData.date !== undefined)
+    return displayData(props.apodData)
+  else if (props.apodError.message !== undefined)
+    return displayError(props.apodError)
+  else
+    return <></>
+}
+
+export default DisplayApod
--- src/components/GetAndDisplayApod.js
+++ src/components/GetAndDisplayApod.js
@@ -3,17 +3,6 @@ import axios from "axios"
 import DisplayApod from "./DisplayApod"
 
 function GetAndDisplayApod() {
-    // const localApodData =
-    // {
-    // "copyright":"Alan FriedmanAverted Imagination",
-    // "date":"2012-03-14",
-    // "explanation":"It's one of the baddest sunspot regions in years. Active Region 1429 may not only look, to some, like an angry bird -- it has thrown off some of the most powerful flares and coronal mass ejections of the current solar cycle. The extended plumes from these explosions have even rained particles on the Earth's magnetosphere that have resulted in colorful auroras. Pictured above, AR 1429 was captured in great detail in the Sun's chromosphere three days ago by isolating a color of light emitted primarily by hydrogen. The resulting image is shown in inverted false color with dark regions being the brightest and hottest. Giant magnetically-channeled tubes of hot gas, some longer than the Earth, are known as spicules and can be seen carpeting the chromosphere. The light tendril just above AR 1429 is a cool filament hovering just over the active sunspot region. As solar maximum nears in the next few years, the increasingly wound and twisted magnetic field of the Sun may create even more furious active regions that chirp even more energetic puffs of solar plasma into our Solar System.   Gallery: See images of the latest planetary conjunction",
-    // "hdurl":"https://apod.nasa.gov/apod/image/1203/angrysun_friedman_1080.jpg",
-    // "media_type":"image",
-    // "service_version":"v1",
-    // "title":"Angry Sun Erupting",
-    // "url":"https://apod.nasa.gov/apod/image/1203/angrysun_friedman_960.jpg"
-    // }
     const [apodData, setApodData] = useState([])
     const [apodError, setApodError] = useState([])
 
@@ -21,13 +10,9 @@ function GetAndDisplayApod() {
         axios
             .get('https://api.nasa.govZZZ/planetary/apod?api_key=DEMO_KEY&date=2012-03-14')
             .then(responseApod => setApodData(responseApod.data))
-            .catch(errorApod => {
-                console.log('getError',errorApod)
-                setApodError(errorApod)
-            })
+            .catch(errorApod => setApodError(errorApod))
         }, [])
-
-    return <div><DisplayApod apodData={apodData} apodError={apodError} /></div>
+    return <DisplayApod apodData={apodData} apodError={apodError} />
 }
 
 export default GetAndDisplayApod
